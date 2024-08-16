b7375b686e1f090dc534c69dcc384343c0a22d1e
Kihoon Han <kihoon.han@furiosa.ai>
2024-08-16 19:58:49 +0900

    modify existing logic to select best one by every loop iteration

--- pkg/npu_allocator/bin_packing_allocator.go
+++ pkg/npu_allocator/bin_packing_allocator.go
@@ -1,5 +1,7 @@
 package npu_allocator
 
+import "sync"
+
 var _ NpuAllocator = (*binPackingNpuAllocator)(nil)
 
 type binPackingNpuAllocator struct {
@@ -35,13 +37,7 @@ func NewBinPackingNpuAllocator(devices DeviceSet) (NpuAllocator, error) {
 		return nil, err
 	}
 
-	return &binPackingNpuAllocator{
-		hintProvider: newTopologyHintProviderForBinPackingAllocator(topologyHintMatrix),
-	}, nil
-}
-
-func newTopologyHintProviderForBinPackingAllocator(topologyHintMatrix TopologyHintMatrix) TopologyHintProvider {
-	return func(device1, device2 Device) uint {
+	hintProvider := func(device1, device2 Device) uint {
 		key1, key2 := device1.GetTopologyHintKey(), device2.GetTopologyHintKey()
 		if key1 > key2 {
 			key1, key2 = key2, key1
@@ -55,8 +51,97 @@ func newTopologyHintProviderForBinPackingAllocator(topologyHintMatrix TopologyHi
 
 		return 0
 	}
+
+	return &binPackingNpuAllocator{hintProvider: hintProvider}, nil
 }
 
 func (b *binPackingNpuAllocator) Allocate(available DeviceSet, required DeviceSet, request int) DeviceSet {
-	return nil
+	subsetLen := request - len(required)
+	// If subsetLen is zero, it means pre-allocated devices already satisfies device request quantity.
+	if subsetLen <= 0 {
+		return required
+	}
+
+	// difference contains devices in `available` set, excluding `required` set.
+	difference := available.Difference(required)
+	remainingDevicesByHintMap := make(map[TopologyHintKey]DeviceSet)
+	for _, device := range difference {
+		topologyHintKey := device.GetTopologyHintKey()
+		if _, ok := remainingDevicesByHintMap[topologyHintKey]; !ok {
+			remainingDevicesByHintMap[topologyHintKey] = make(DeviceSet, 0)
+		}
+
+		remainingDevicesByHintMap[topologyHintKey] = append(remainingDevicesByHintMap[topologyHintKey], device)
+	}
+
+	// finalizedDevices contains finalized allocated devices.
+	finalizedDevices := make(DeviceSet, 0, request)
+	finalizedDevices = finalizedDevices.Union(required)
+
+	for subsetLen > 0 {
+		selectedDevices := b.selectBestScoredDevices(subsetLen, finalizedDevices, remainingDevicesByHintMap)
+		subsetLen -= len(selectedDevices)
+		finalizedDevices = finalizedDevices.Union(selectedDevices)
+	}
+
+	return finalizedDevices
+}
+
+// scoreDeviceSet returns total sum of scores for each pair of devices.
+func (b *binPackingNpuAllocator) scoreDeviceSet(deviceSet DeviceSet) uint {
+	var total uint = 0
+	for i := 0; i < len(deviceSet); i++ {
+		for j := i + 1; j < len(deviceSet); j++ {
+			total += b.scoreDevicePair(deviceSet[i], deviceSet[j])
+		}
+	}
+
+	return total
+}
+
+// scoreDevicePair returns score based on distance between two devices.
+// Higher score means lower distance.
+func (b *binPackingNpuAllocator) scoreDevicePair(device1 Device, device2 Device) uint {
+	return b.hintProvider(device1, device2)
+}
+
+func (b *binPackingNpuAllocator) selectBestScoredDevices(subsetLen int, finalizedDevices DeviceSet, remainingDevicesByHintMap map[TopologyHintKey]DeviceSet) DeviceSet {
+	var highestScore uint = 0
+	var selectedHintKey TopologyHintKey = ""
+
+	wg := new(sync.WaitGroup)
+	lock := new(sync.Mutex)
+
+	for topologyHintKey, devices := range remainingDevicesByHintMap {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			partialDevices := devices
+			if len(devices) > subsetLen {
+				partialDevices = devices[:subsetLen]
+			}
+
+			score := b.scoreDeviceSet(partialDevices.Union(finalizedDevices))
+
+			lock.Lock()
+			if selectedHintKey == "" || highestScore < score {
+				highestScore = score
+				selectedHintKey = topologyHintKey
+			}
+			lock.Unlock()
+		}()
+	}
+
+	wg.Wait()
+
+	selectedDevices := remainingDevicesByHintMap[selectedHintKey]
+	if len(selectedDevices) > subsetLen {
+		selectedDevices = selectedDevices[:subsetLen]
+		remainingDevicesByHintMap[selectedHintKey] = remainingDevicesByHintMap[selectedHintKey][subsetLen:]
+	} else {
+		delete(remainingDevicesByHintMap, selectedHintKey)
+	}
+
+	return finalizedDevices.Union(selectedDevices)
 }
